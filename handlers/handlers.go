6d404952d67c04cd71d57e0d1daa8ef0df38fa40
renuramesh <renu.p@techversantinfo.com>
2022-05-16 15:05:06 +0530

    handler package

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -9,6 +9,7 @@ import (
 )
 
 type Handler struct {
+	flag bool
 }
 
 func NewHandler() Handler {
--- main.go
+++ main.go
@@ -2,15 +2,15 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
-	"github.com/renu-ramesh/robot-apocalypse-docker/handlers"
+	handlers "github.com/renu-ramesh/robot-apocalypse-docker/handlers"
 )
 
 func main() {
-	h := handlers.NewHandler()
+	var h handlers.Handler
 	router := gin.Default()
 	router.GET("/albums", h.getAlbums)
-	router.GET("/albums/:id", h.getAlbumByID)
-	router.POST("/albums", h.postAlbums)
+	// router.GET("/albums/:id", h.getAlbumByID)
+	// router.POST("/albums", h.postAlbums)
 
 	router.Run("localhost:8080")
 }
